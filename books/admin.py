1c37d7a3864da6603321772a5e4e79f3e316bd33
Abram <israelvictory87@gmail.com>
2022-09-02 16:29:36 +0100

    Registered Book and Author to admin

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,15 @@
+# Django Imports
 from django.contrib import admin
 
-# Register your models here.
+# Own Imports
+from books.models import Book, Author
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "isbn", "author"]
+    
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ["id", "first_name", "last_name"]
