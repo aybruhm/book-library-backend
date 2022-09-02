8374d1fd8a1ca187510e6eac31b2b54a59434645
Abram <israelvictory87@gmail.com>
2022-09-02 16:29:20 +0100

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -33,7 +33,6 @@ urlpatterns = [
     # api documentation endpoints
     re_path(r'^generate_api_documentation(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 if settings.DEBUG:
