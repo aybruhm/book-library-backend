10c57c7dc2e41686fbecd76598bff8c5f29f7cba
Abram <israelvictory87@gmail.com>
2022-09-02 15:47:22 +0100

    Created create and update methods for AuthorSerializer

--- books/serializers.py
+++ books/serializers.py
@@ -10,9 +10,15 @@ class AuthorSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Author
-        fields = ("id", "first_name", "last_name")
+        fields = ("id", "first_name", "last_name")      
         
-        
+    def create(self, validated_data:dict):
+        author = Author.objects.create(**validated_data)
+        author.save()
+        return author
+    
+    def update(self, instance, validated_data):
+        return super().update(instance, validated_data)
 
 class BookSerializer(serializers.ModelSerializer):
     author = AuthorSerializer(read_only=True)
@@ -20,3 +26,4 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ("id", "name", "isbn", "author")
+    
