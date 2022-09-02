89f38db6cb2c001c38b010c52f4be2dec686591d
Abram <israelvictory87@gmail.com>
2022-09-02 15:53:25 +0100

    Update books/urls.py

--- books/urls.py
+++ books/urls.py
@@ -10,12 +10,12 @@ from books.views import (
 
 
 urlpatterns = [
-    # books endpoints
+    # fetch endpoints
     path("books/", BooksAPIView.as_view(), name="books"),
-    path("book/<int:id>/", GetBookAPIView.as_view(), name="book"),
-    
-    # authors endpoints
     path("authors/", AuthorAPIView.as_view(), name="authors"),
+    
+    # get detail and put endpoints
+    path("book/<int:id>/", GetBookAPIView.as_view(), name="book"),
     path("author/<int:id>/", GetAuthorAPIView.as_view(), name="author"),
     
     # create new resource endpoints
