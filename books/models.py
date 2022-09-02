350dbcd0d2c03cacff67541e866193469a322613
Abram <israelvictory87@gmail.com>
2022-09-02 15:11:45 +0100

    Created Author and Book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    first_name = models.TextField()
+    last_name = models.TextField()
+    
+    class Meta:
+        verbose_name_plural = "Authors"
+        db_table = "authors"
+        
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
+class Book(models.Model):
+    name = models.TextField()
+    isbn = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    
+    class Meta:
+        verbose_name_plural = "Books"
+        db_table = "books"
+        
+    def __str__(self) -> str:
+        return self.name
