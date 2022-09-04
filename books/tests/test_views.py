04b315192e318008550c179fd2ffae8c3888b1ef
Abram <israelvictory87@gmail.com>
2022-09-04 09:49:57 +0100

    Created book test cases

--- books/tests/test_views.py
+++ books/tests/test_views.py
@@ -29,23 +29,25 @@ class BooksTestCase(APITestCase):
         Book.objects.create(name="Clean Code", isbn="0875754570", author=Author.objects.get_or_create(id=2)[0])
     
     def test_get_all_books(self):
-        # get response
-        response = client.get(reverse("books"))
+        """
+        Test that the response data from the API is equal 
+        to the serialized data of all thebooks in the database
         
-        # get data from database
+        :return: A response object data and status_code 200
+        """
+        response = client.get(reverse("books"))
+    
         books = Book.objects.all()
         
-        # serializer books
         serializer = BookSerializer(books, many=True)
         serializer_data = success_response(status=True, message="Books retrieved!", data=serializer.data)
         
-        # checks if response and serializer data are the same,
-        # and the status code is 200 ok
         self.assertEqual(response.data, serializer_data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         
         
 class BookTestCase(APITestCase):
+    """Test case to create and get a single book api"""
     
     def setUp(self) -> None:
         self.author = Author.objects.create(first_name="John", last_name="Doe")
@@ -68,6 +70,12 @@ class BookTestCase(APITestCase):
         }
     
     def test_get_single_book(self):
+        """
+        Test that the response data from the API is equal 
+        to the serialized data of the book object
+        
+        :return: A response object data and status_code 200
+        """
         response = client.get(reverse('book', args=[self.book.id]))        
         book = Book.objects.get(id=self.book.id)
         
@@ -78,6 +86,12 @@ class BookTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
     
     def test_create_valid_book(self):
+        """
+        Test case to send a POST request to the create_book 
+        endpoint with a valid payload
+        
+        :return: A response status_code 201
+        """
         response = client.post(
             reverse('create_book'),
             data=json.dumps(self.valid_payload),
@@ -86,6 +100,12 @@ class BookTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         
     def test_create_invalid_book(self):
+        """
+        Test case to send a POST request to the create_book 
+        endpoint with an invalid payload
+        
+        :return: A response status_code 400
+        """
         response = client.post(
             reverse('create_book'),
             data=json.dumps(self.invalid_payload),
@@ -95,6 +115,7 @@ class BookTestCase(APITestCase):
         
 
 class UpdateSingleBookTestCase(APITestCase):
+    """Test case to update a single book api"""
     
     def setUp(self) -> None:
         self.author = Author.objects.create(first_name="John", last_name="Doe")
@@ -117,6 +138,12 @@ class UpdateSingleBookTestCase(APITestCase):
         }
     
     def test_valid_update_single_book(self):
+        """
+        Test case to send a PUT request to the book endpoint with the book id, 
+        and the valid payload
+        
+        :return: A response status_code 200
+        """
         response = client.put(
             reverse('book', args=[self.book.id]),
             data=json.dumps(self.valid_payload),
@@ -125,6 +152,11 @@ class UpdateSingleBookTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         
     def test_invalid_update_single_book(self):
+        """
+        Test case to update a book with invalid data
+        
+        :return: A response status_code 400
+        """
         response = client.put(
             reverse('book', args=[self.book.id]),
             data=json.dumps(self.invalid_payload),
@@ -133,6 +165,11 @@ class UpdateSingleBookTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         
     def test_update_single_book_not_found(self):
+        """
+        Test case to update a book that doesn't exist
+        
+        :return: A response status_code 404
+        """
         response = client.put(
             reverse('book', args=[53]),
             data=json.dumps(self.valid_payload),
